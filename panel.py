4e66758ba460ff472590b6f5e4f80c89f76be09e
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-05-16 15:34:09 +0200

    continued panel

--- panel.py
+++ panel.py
@@ -19,40 +19,39 @@ class Panel:
         max_height, max_width = self.parent.getmaxyx()
         vis_height = min(self.height, max(max_height - self.top,  0))
         vis_width  = min(self.width,  max(max_width  - self.left, 0))
-        #
-        #             /'''''' height
-        #            / .     
-        #           /  .     
-        #          /   .     
-        #         /    .     
-        #        /     .     
-        # 0 ,,,,/      .     
-        #   ----+------+-----------> max_height
-        #       top    top+height
-        #
+        #  
+        #        A vis_height
+        #        |
+        # height + . . . . . . ,------
+        #        |            / .     
+        #        |           /  .     
+        #        |          /   .     
+        #        |         /    .     
+        #        |        /     .     
+        #      0 +-------´      .     
+        #        L-------+------+-----------> max_height
+        #                top    top+height
 
         if vis_height == 0 or vis_width == 0:
             self.win = None
             return
 
-        if vis_height == 0:
-            isNewWindow = False
+        if self.win is None:
+            recreate = True
+        else: # self.win exists
+            sub_height, sub_width = self.win.getmaxyx()
+            sub_top,    sub_left  = self.win.getparyx()
 
-        else: # vis_height > 0
-            if self.win is None:
-                recreate = True
-            else: # self.win exists
-                sub_height, sub_width = self.win.getmaxyx()
-                sub_top, sub_left = self.win.getparyx()
-
-                recreate = any(sub_height != vis_height,
-                               sub_width  != vis_width,
-                               self.top   != sub_top,
-                               self.left  != sub_left)
-                if recreate:
-                    self.win = self.parent.subwin(vis_height, vis_width,
-                                                  self.top, self.left)
-            isNewWindow = recreate
+            recreate = any(sub_height != vis_height,
+                           sub_width  != vis_width,
+                           sub_top    != self.top,
+                           sub_left   != self.left)
+        if recreate:
+            self.win = self.parent.subwin(vis_height, vis_width,
+                                          self.top, self.left)
+        self.win.erase()
+        self.draw(vis_height, vis_width)
+        self.win.refresh()
 
                             
 
