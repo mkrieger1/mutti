3688f75462de207b732516b4ceec3c117c61472f
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-05-23 17:59:48 +0200

    simplified screen

--- screen.py
+++ screen.py
@@ -1,18 +1,25 @@
 import curses
-from panel import Panel
+from panel import Panel, PanelError
 
 class Screen(Panel):
     def __init__(self, stdscr):
         Panel.__init__(self, win=stdscr)
         self.set_focus(True)
 
+    def adopt(self, child):
+        if not self.children:
+            Panel.adopt(self, child)
+        else:
+            raise PanelError('can only have one child')
+
     def _update_size(self):
         c = self.children[0]
         return (c.min_height, c.min_width)
 
     def _layout(self, height, width):
-        self.children[0].give_window(height=height-2, width=width-2,
-                                     top=1, left=1)
+        c = self.children[0]
+        self.children[0].give_window()
+
     def _erase(self, height, width):
         self.win.erase()
 
