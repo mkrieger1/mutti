0cb93fe8fe9459a32902a7ea60ef0dee731bb0a1
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-05-31 19:49:27 +0200

    restructured main (new: main_loop() and build_panels())

--- main.py
+++ main.py
@@ -8,73 +8,104 @@ from lists import PanelVList, PanelHList
 from status import Status
 from align import VAlign, HAlign
 from toggle import Toggle
+from grid import Grid
+from tabs import Tabs
 
+#--------------------------------------------------------------------
 
-def main(stdscr, *main_args):
-    curses.use_default_colors()
+def build_panels(stdscr):
     f = open('loglog.log', 'w')
     mainscreen = Screen(stdscr)
+    mainscreen._log = f
 
-    statusbar = Status(width=80)
+    statusbar = mainscreen.statusbar
     statusbar._log = f
-    status_bottom = VAlign(align='bottom', height=30)
-    status_bottom.adopt(statusbar)
 
-    hlisttop = PanelHList()
+    tabs = Tabs()
 
     vlist = PanelVList()
-    vlist._log = f
+    #vlist._log = f
     for i in range(5):
         d = Dial("Dial %02i"%i, (-100, 100), 6, 2*i)
         d._status = statusbar
-        d._log = f
+        #d._log = f
         vlist.adopt(d)
-    vlist.adopt(status_bottom)
 
     hlist = PanelHList()
-    hlist._log = f
+    #hlist._log = f
     for i in range(3):
         d = Dial("Dial %02i"%i, (-100, 100), 6, 2*i)
         d._status = statusbar
         hlist.adopt(d)
-    hlist_bottom = VAlign(align='center', height=20)
-    hlist_bottom.adopt(hlist)
-    vlist.adopt(hlist_bottom)
+    hlist_align = VAlign(align='center', height=20)
+    hlist_align.adopt(hlist)
+    vlist.adopt(hlist_align)
 
     vlist2 = PanelVList()
-    vlist2._log = f
+    #vlist2._log = f
     for i in range(5):
         t = Toggle("Toggle %02i"%i)
         t._status = statusbar
-        t._log = f
+        #t._log = f
         vlist2.adopt(t)
     for i in range(5):
         t = Toggle("ToToToggle %02i"%i)
         t._status = statusbar
-        t._log = f
+        #t._log = f
         vlist2.adopt(t)
     for i in range(5):
         t = Toggle("InvisibleToggle %02i"%i, draw_label=False)
         t._status = statusbar
-        t._log = f
+        #t._log = f
         vlist2.adopt(t)
 
-    hlisttop.adopt(vlist)
-    hlisttop.adopt(vlist2)
+    rows, cols = 10, 10
+    g = Grid(rows, cols)
+    #g._log = f
+    for row in range(rows):
+        for col in range(cols):
+            if not row == col:
+                t = Toggle("Tog%i%i" % (row, col), draw_label=True)
+                t._status = statusbar
+                g.adopt(t, row, col)
+            else:
+                d = Dial("Dial%i%i" % (row, col), (-32, 32), 3)
+                d._status = statusbar
+                g.adopt(d, row, col)
 
-    mainscreen.adopt(hlisttop)
-    mainscreen._log = f
+    tabs.adopt(vlist, "first VList")
+    tabs.adopt(vlist2, "second VList")
+    tabs.adopt(g, "Grid")
+    tabs._log = f
+    
+    vlist_main = PanelVList()
+    vlist_main.adopt(tabs)
 
+    mainscreen.adopt(vlist_main)
 
+    return mainscreen
+
+#--------------------------------------------------------------------
+
+def main_loop(top_panel):
+    """
+    Run the application.
+
+    top_panel must be a Panel instance with top_panel.win == stdscr.
+    """
     while 1:
-        mainscreen.redraw()
+        top_panel.redraw()
         curses.doupdate()
-        key = stdscr.getch()
+        key = top_panel.win.getch()
         try:
-            mainscreen.handle_key(key)
+            top_panel.handle_key(key)
         except QuitScreen:
             break
-            
+
+def main(stdscr, *main_args):
+    curses.use_default_colors()
+    top_panel = build_panels(stdscr)
+    main_loop(top_panel)
 
 if __name__=='__main__':
     try:
