2196f1794b0885f2684a4f551852c0f81d07563b
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-06-04 13:19:39 +0200

    added HSpace, VSpace

---
+++ space.py
@@ -0,0 +1,26 @@
+from panel import Panel
+
+
+class VSpace(Panel):
+    """
+    Empty panel which occupies vertical space.
+    """
+    _max_children = 0
+    _focusable = False
+
+    def __init__(self, height):
+        Panel.__init__(self, min_height=height, max_height=height,
+                             max_width=0)
+
+
+class HSpace(Panel):
+    """
+    Empty panel which occupies horizontal space.
+    """
+    _max_children = 0
+    _focusable = False
+
+    def __init__(self, width):
+        Panel.__init__(self, min_width=width, max_width=width,
+                             max_height=0)
+
