3192662dd2aa70dea808653e00914249b825e486
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-06-05 11:39:13 +0200

    improved mutti/__init__.py

--- mutti/__init__.py
+++ mutti/__init__.py
@@ -1,2 +1,25 @@
+from align  import HAlign, VAlign
+from dial   import Dial
+from grid   import Grid
+from label  import Label
+from lists  import HList, VList
+from screen import Screen, QuitScreen
+from space  import HSpace, VSpace
+from tabs   import Tabs
+from toggle import Toggle
+
+del align 
+del dial  
+del grid  
+del label 
+del lists 
+del screen
+del space 
+del tabs  
+del toggle
+
 __version__ = '0.1.0'
+
+__all__ = ['HAlign', 'VAlign', 'Dial', 'Grid', 'Label', 'HList', 'VList',
+           'Screen', 'QuitScreen', 'HSpace', 'VSpace', 'Tabs', 'Toggle']
 
